b8b62ae94bda88a4889d2ab5889ea5f8b8a80033
Shukhrat <losteand@yandex.ru>
2025-09-08 19:12:49 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Alice", "123"));
+        assertEquals(2, phoneBook.add("Bob", "456"));
+        assertEquals(2, phoneBook.add("Alice", "789"));
+    }
 }
